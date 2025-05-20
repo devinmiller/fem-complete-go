82577a7703886c2a7a869352feae93390dd79a49
Devin Miller <devinmiller@codeonthebrain.com>
2025-05-19 17:02:02 -0700

    Continuing to work on course

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/devinmiller/fem-complete-go
 
 go 1.24.3
+
+require github.com/go-chi/chi/v5 v5.2.1 // indirect
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,1 +1,17 @@
 package routes
+
+import (
+	"github.com/devinmiller/fem-complete-go/internal/app"
+	"github.com/go-chi/chi/v5"
+)
+
+func SetupRoutes(app *app.Application) *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Get("/health", app.HealthCheck)
+	r.Get("/worksouts/{id}", app.WorkoutHandler.HandleGetWorkoutById)
+
+	r.Post("/workouts", app.WorkoutHandler.HandleCreateWorkout)
+
+	return r
+}
