fcba47444ccc14371d11bb02607497067cb20ce8
Devin Miller <devinmiller@codeonthebrain.com>
2025-05-30 16:28:12 -0700

    working through course

---
+++ internal/api/user_handler.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"errors"
+	"log"
+
+	"github.com/devinmiller/fem-complete-go/internal/store"
+)
+
+type registerUserRequest struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Bio      string `json:"bio"`
+}
+
+type UserHandler struct {
+	userStore store.UserStore
+	logger    *log.Logger
+}
+
+func NewUserhandler(userStore store.UserStore, logger *log.Logger) *UserHandler {
+	return &UserHandler{
+		userStore: userStore,
+		logger:    logger,
+	}
+}
+
+func (h *UserHandler) validateRegisterRequest(req *registerUserRequest) error {
+	if req.Username == "" {
+		return errors.New("username is required")
+	}
+}
